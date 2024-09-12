572a8c4569aea36b49824cdee995c2c79d0c9e49
Marco Olivo <8478776+olivomarco@users.noreply.github.com>
2024-09-12 13:32:39 +0200

    Changes

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # Node.js Hello World
 
+This simple web application exposes different types of content:
+
+- static content (HTML, CSS, JS, images) served by the web server, under folder [src/client](src/client)
+- dynamic, local APIs, exposed by the [src/server/server.js](src/server/server.js) file (for example, the `/local-api/hello` endpoint for a simple hello-world)
+- dynamic, remote APIs, exposed by the [src/server/server.js](src/server/server.js) file (for example, the `/api/get` endpoint which fetches data from a remote API exposed by [httpbin](https://httpbin.org/))
+
+## Develop and test locally
+
+To develop and test locally:
+
+```bash
+npm install
+npm run start:dev
+```
+
+and then connect to `http://localhost:9000` to see the static webpages, and connect to `http://localhost:3000` to see dynamic (nodejs) backend content.
+
+## Deploy
+
 Deploy this code to Azure App Service with the following command:
 
 ```bash
@@ -11,10 +30,13 @@ az group create --name $RESOURCEGROUP --location italynorth
 az appservice plan create --name $APPNAME --resource-group $RESOURCEGROUP --sku B1 --is-linux
 az webapp create --name $APPNAME --resource-group $RESOURCEGROUP --plan $APPNAME --runtime "NODE|18-lts"
 
+# set NODE_ENV to production in the app settings
+az webapp config appsettings set --name $APPNAME --resource-group $RESOURCEGROUP --settings NODE_ENV=production
+
 # deploy the code via zip-deploy
-npm install
-rm -f $APPNAME.zip ; zip -r $APPNAME.zip .
-az webapp deployment source config-zip -g $RESOURCEGROUP -n $APPNAME --src $APPNAME.zip && rm $APPNAME.zip
+npx webpack --config webpack.config.js --mode production
+npm install && npm run build && npm run zip
+az webapp deployment source config-zip -g $RESOURCEGROUP -n $APPNAME --src $APPNAME.zip && rm app-service-hello-world.zip
 
 # verify the deployment
 curl https://$APPNAME.azurewebsites.net # hello world from index.js
--- src/server/server.js
+++ src/server/server.js
@@ -7,97 +7,43 @@ const path = require("path");
 //const packageJson = require(packageJsonPath);
 const config = require('./config'); // Adjust path if `config.js` is in a different directory
 const packageJson = require(config.packageJsonPath);
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // App constants
 const port = process.env.PORT || 3000;
-const apiPrefix = "/api";
-
-// Store data in-memory, not suited for production use!
-const db = {
-  test: {
-    user: "test",
-    currency: "$",
-    description: `Test account`,
-    balance: 75,
-    transactions: [
-      { id: "1", date: "2020-10-01", object: "Pocket money", amount: 50 },
-      { id: "2", date: "2020-10-03", object: "Book", amount: -10 },
-      { id: "3", date: "2020-10-04", object: "Sandwich", amount: -5 },
-    ],
-  },
-  jondoe: {
-    user: "jondoe",
-    currency: "$",
-    description: `Second test account`,
-    balance: 150,
-    transactions: [
-      { id: "1", date: "2022-10-01", object: "Gum", amount: -2 },
-      { id: "2", date: "2022-10-03", object: "Book", amount: -10 },
-      { id: "3", date: "2022-10-04", object: "Restaurant", amount: -45 },
-    ],
-  },
-};
 
 // Create the Express app & setup middlewares
 const app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(cors({ origin: /http:\/\/(127(\.\d){3}|localhost)/ }));
-// app.options("*", cors());
 
-// // ***************************************************************************
-
-// // Configure routes
-// const router = express.Router();
-
-// // Hello World for index page
-// app.get("/", function (req, res) {
-//   return res.send("Hello World!");
-// });
-
-// app.get("/api", function (req, res) {
-//   return res.send("Fabrikam Bank API");
-// });
-
-// //app.use("/public", express.static(path.join(__dirname, ".")));
-
-// // Serve index.html for all other routes (so front-end routing works)
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-// // ----------------------------------------------
-
-// // Get all data for the specified account
-// router.get("/accounts/:user", (req, res) => {
-//   const account = db[req.params.user];
-
-//   // Check if account exists
-//   if (!account) {
-//     return res.status(404).json({ error: "User does not exist" });
-//   }
-
-//   return res.json(account);
-// });
-
-// // ***************************************************************************
-
-// // Add 'api` prefix to all routes
-// app.use(apiPrefix, router);
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server listening on port ${port}`);
-// });
-
-
+// Proxy configuration
+const API_URL = 'https://httpbin.org/'; // External API URL
+ 
+// Proxy middleware
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: API_URL,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '', // Remove /api prefix when forwarding to the target server
+    },
+    onProxyReq: (proxyReq, req, res) => {
+         },
+    onProxyRes: (proxyRes, req, res) => {
+         },
+    onError: (err, req, res) => {
+      console.error(`Proxy error: ${err.message} for ${req.method} ${req.url}`);
+      res.status(500).send('Proxy error');
+    },
+  })
+);
 
 // Serve static files from the "build" directory
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-// Handle API requests (your backend logic)
-app.get('/api/hello', (req, res) => {
-  console.log('Received request on /api/hello');
+// Handle local API requests (your backend logic)
+app.get('/local-api/hello', (req, res) => {
+  console.log('Received request on /local-api/hello');
   res.send({ message: 'Hello from the backend!' });
 });
 
